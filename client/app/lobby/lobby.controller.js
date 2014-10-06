a10a0c03c4cf0626a885f6a24e57e4d9f7864cd6
Siroberto Scerbo <github@siroberto.com>
2014-10-06 14:17:05 -0400

    added basic lobby functionality

--- client/app/lobby/lobby.controller.js
+++ client/app/lobby/lobby.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('7wondersApp')
-  .controller('LobbyCtrl', function ($scope) {
+  .controller('LobbyCtrl', function ($scope, Auth) {
     $scope.message = 'Hello';
+
+    $scope.joinGame = function () {
+        console.log(Auth.getCurrentUser().name + " (" + Auth.getCurrentUser()._id + ") wants to join a game");
+    };
   });
