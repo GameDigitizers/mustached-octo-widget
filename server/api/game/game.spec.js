75e095f30aff5332392b54d27bad1a79a63e5410
Siroberto Scerbo <github@siroberto.com>
2014-10-06 20:26:02 -0400

    test for join game logic

--- server/api/game/game.spec.js
+++ server/api/game/game.spec.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var should = require('should');
-var app = require('../../app');
-var request = require('supertest');
+var should = require('should'),
+    app = require('../../app'),
+    request = require('supertest'),
+    mongoose = require('mongoose'),
+    Game = mongoose.model('Game');
 
 describe('GET /api/games', function() {
 
@@ -14,6 +16,39 @@ describe('GET /api/games', function() {
       .end(function(err, res) {
         if (err) return done(err);
         res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+});
+
+describe('POST /api/games/new/:name', function() {
+
+  before(function (done) {
+    console.log("Removing all games");
+    Game.remove().exec();
+    done();
+  });
+
+  it('should respond with a 201 with game if name does not exist yet', function(done) {
+    request(app)
+      .post('/api/games/new/octo-gamer')
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
+        done();
+      });
+  });
+
+  it ('should respond with a 200 if the game exists', function (done) {
+    request(app)
+      .post('/api/games/new/octo-gamer')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
         done();
       });
   });
