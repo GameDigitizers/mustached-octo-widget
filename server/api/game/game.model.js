4655fa0296929a6ec7650f0c64fb11c74d57f510
Siroberto Scerbo <github@siroberto.com>
2014-10-06 22:58:50 -0400

    basic game model

--- server/api/game/game.model.js
+++ server/api/game/game.model.js
@@ -1,75 +1,80 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    UserSchema = require('../user/user.model.js')
-
-var BuildingSchema = new Schema({
-    name: String,
-    cost: {
-        type: Schema.ObjectId,
-        ref: 'ResourceSchema'
-    },
-    grants: [String]
-});
-
-var ResourceSchema = new Schema({
-    name: String
-});
+  util = require('util'),
+  Schema = mongoose.Schema,
+  UserSchema = require('../user/user.model.js'),
+  BuildingSchema = require('../building/building.model.js')
 
 var WonderSchema = new Schema({
-    name: String,
-    buildings: {
-        type: Schema.ObjectId,
-        ref: 'BuildingSchema'
-    },
-    resources: {
-        type: Schema.ObjectId,
-        ref: 'ResourceSchema'
-    }
+  name: String,
+  resource: {
+    type: Schema.ObjectId,
+    ref: 'CardSchema',
+  },
+  stages: [Object]
 });
 
-var CardSchema = new Schema({
-    name: String,
-    builds: {
-        type: Schema.ObjectId,
-        ref: 'CardSchema',
-    },
-    built_by: {
-        type: Schema.ObjectId,
-        ref: 'CardSchema'
-    }
-});
+// var BuildingSchema = new Schema({
+//   name: String,
+//   type: {
+//     type: String, 
+//     enum: [
+//     'Raw Materials',
+//     'Manufactured Goods',
+//     'Military',
+//     'Civilian',
+//     'Scientific',
+//     'Commercial',
+//     'Guild'
+//     ]
+//   },
+//   builds: {
+//     type: Schema.ObjectId,
+//     ref: 'BuildingSchema',
+//   },
+//   built_by: {
+//     type: Schema.ObjectId,
+//     ref: 'BuildingSchema'
+//   },
+//   cost: Object,
+//   grants: Object
+// });
 
 var PlayerSchema = new Schema({
-    money: Number,
-    cards: [{
-        type: Schema.ObjectId,
-        ref: 'CardSchema'
-    }],
-    wonder: {
-        type: Schema.ObjectId,
-        ref: 'WonderSchema'
-    },
-    user: {
-        type: Schema.ObjectId,
-        ref: 'UserSchema'
-    }
+  user: {
+    type: Schema.ObjectId,
+    ref: 'UserSchema'
+  },
+  money: Number,
+  military: {
+    'wins': [Number],
+    'loses': Number
+  },
+  hand: [{
+    type: Schema.ObjectId,
+    ref: 'BuildingSchema'
+  }],
+  wonder: {
+    type: Schema.ObjectId,
+    ref: 'WonderSchema'
+  },
+  buildings: [{
+    type: Schema.ObjectId,
+    ref: 'BuildingSchema'
+  }]
 });
 
 var GameSchema = new Schema({
-    name: String,
-    active: Boolean,
-    players: {
-        type: Schema.ObjectId,
-        ref: 'PlayerSchema'
-    }
+  name: String,
+  active: Boolean,
+  players: {
+    type: Schema.ObjectId,
+    ref: 'PlayerSchema'
+  }
 });
 
-/**
- * Validations
- */
-
+// Validations
 var Game = mongoose.model('Game', GameSchema);
 
 Game.schema.path('name').validate(function (name) {
